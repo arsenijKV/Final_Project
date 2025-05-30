b854f260cefed6c1ee34993a24373e80078134b1
arsenijKV <arsenij.kononoff@gmail.com>
2025-05-30 21:44:27 +0300

    90_procent

--- logic.py
+++ logic.py
@@ -8,7 +8,7 @@ class DB_Manager:
     def create_tables(self):
         conn = sqlite3.connect(self.database)
         with conn:
-            conn.execute('''CREATE TABLE questions(
+            conn.execute('''CREATE TABLE question(
                                     id INTEGER PRIMARY KEY,
                                     questions TEXT NOT NULL,
                                     tags TEXT
@@ -19,8 +19,15 @@ class DB_Manager:
 
             conn.commit()
 
-
-
+            
+        
+            conn.execute('''
+                        CREATE TABLE user_answers (
+                            user_id INTEGER,
+                            question_key INTEGER,
+                            answer TEXT,
+                            FOREIGN KEY(question_key) REFERENCES question(id)
+                        )''')
 
     def __executemany(self, sql, data):
         conn = sqlite3.connect(self.database)
@@ -35,6 +42,44 @@ class DB_Manager:
             cur.execute(sql, data)
             return cur.fetchall()
         
+    def get_all_quest(self):
+        sql = 'SELECT id, questions FROM question'
+        return self.__select_data(sql)
+    
+    def save_answer(self, user_id, key, answer):
+        sql = 'INSERT INTO user_answers (user_id, question_key, answer) VALUES (?, ?, ?)'
+        self.__executemany(sql, [(user_id, key, answer)])
+
+    def insert_quest(self):
+        all_questions = {
+            "learn": "Где бы ты хотел работать(учиться)?",
+            "job_wear": "Какой стиль работы ты предпочетаешь?",
+            "skills": "Какие скилы(умения) ты имеешь?",
+            "internship": "Где ты раньше учился?",
+            "kind": "Что тебе важно в работе?",
+            "sfera": "Есть ли у тебя любимая сфера?"
+        }
+
+        data = [(question, key) for key, question in all_questions.items()]
+        sql = 'INSERT INTO question (questions, tags) VALUES (?, ?)'
+        self.__executemany(sql, data)
+
+    def get_user_ansfer(self):
+        sql = 'SELECT answer FROM user_answers'
+        return self.__select_data(sql)
+    
+    def get_user_answer(self, user_id):
+        sql = '''SELECT user_answers.user_id, question.questions, user_answers.answer
+                 FROM user_answers
+                 INNER JOIN question ON user_answers.question_key = question.id
+                 WHERE user_answers.user_id = ?
+                 '''
+        return self.__select_data(sql = sql, data = (user_id,))
+
+
+
+    
+        
 
         
         
@@ -42,3 +87,4 @@ class DB_Manager:
 if __name__ == '__main__':
     manager = DB_Manager(DATABASE)
     manager.create_tables()
+    manager.insert_quest()
--- main.py
+++ main.py
@@ -5,15 +5,11 @@ from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeybo
 from telebot import types
 
 bot = TeleBot(TOKEN)
-user_data = {}
-all_questions = {
-    "learn": "Где бы ты хотел работать(учиться)?",
-    "job_wear": "Какой стиль работы ты предпочетаешь?",
-    "skills": "Какие скилы(умения) ты имеешь?",
-    "internship": "Где ты раньше учился?",
-    "kind": "Что тебе важно в работе?",
-    "sfera": "Есть ли у тебя любимая сфера?"
-}
+manager = DB_Manager(DATABASE)
+
+
+questions_from_db = manager.get_all_quest()
+all_questions = {str(q_id): question_text for q_id, question_text in questions_from_db}
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -37,11 +33,10 @@ def first(call):
     key = call.data
     bot.answer_callback_query(call.id)
     bot.send_message(call.message.chat.id, all_questions[key]) 
-    bot.send_message(call.message.chat.id, 'Ответь на этот вопрос')
     bot.register_next_step_handler_by_chat_id(call.message.chat.id, first_answer, key)
 
 def first_answer(message, answered_key):
-    user_data[message.chat.id] = {answered_key: message.text}
+    manager.save_answer(message.chat.id, answered_key, message.text)
     remaining_questions = {k: v for k, v in all_questions.items() if k != answered_key}
     ask_next_question(message, remaining_questions)
 
@@ -54,7 +49,7 @@ def ask_next_question(message, questions):
     bot.register_next_step_handler_by_chat_id(message.chat.id, handle_next_answer, key, questions)
 
 def handle_next_answer(message, key, questions):
-    user_data[message.chat.id][key] = message.text
+    manager.save_answer(message.chat.id, key, message.text)
     ask_next_question(message, questions)
    
     
@@ -71,5 +66,5 @@ def handle_next_answer(message, key, questions):
 
 
 if __name__ == '__main__':
-    #manager = DB_Manager(DATABASE)
+    manager = DB_Manager(DATABASE)
     bot.infinity_polling()
